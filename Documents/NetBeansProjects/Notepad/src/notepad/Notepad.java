aeab54e73669ec76b4d73c5d1b940b29c1e4b04e
souravg10 <souravgpt10@gmail.com>
2017-06-25 00:42:32 +0545

    Update Notepad.java

--- Documents/NetBeansProjects/Notepad/src/notepad/Notepad.java
+++ Documents/NetBeansProjects/Notepad/src/notepad/Notepad.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
@@ -21,12 +22,16 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.*;//FileNameExtensionFilter;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.Highlighter.HighlightPainter;
+import javax.swing.text.JTextComponent;
 
 /**
  *
  * @author Sourav
  */
-public class Notepad extends JFrame implements ActionListener{
+public final class Notepad extends JFrame implements ActionListener{
 
     /**
      * @param args the command line arguments
@@ -38,6 +43,8 @@ public class Notepad extends JFrame implements ActionListener{
     boolean isFileOpen;
     String FileOpenAddress;
     boolean isSaved=false;
+    ArrayList<String> screentext=new ArrayList<String>();
+    String copytext=null;
         
     public Notepad() throws IOException
     {
@@ -47,10 +54,12 @@ public class Notepad extends JFrame implements ActionListener{
         setSize((7*dm.width)/8,(7*dm.height)/8);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
+            @Override
             public void windowClosing(WindowEvent e) {
                 {
                     int ans=0;
-                    if (!isSaved)
+                    ArrayList<String> text=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
+                    if (!screentext.equals(text))
                     {
                         if (isFileOpen)
                             ans=confirmToSave();
@@ -63,6 +72,7 @@ public class Notepad extends JFrame implements ActionListener{
                             
                             }    
                     }
+                    screentext=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
                     if (ans==0)
                         System.exit(0);
                 }        
@@ -118,6 +128,55 @@ public class Notepad extends JFrame implements ActionListener{
         file.add(exit);
         
         menubar.add(file);
+        
+        JMenu edit=new JMenu("EDIT");
+        edit.setMnemonic('E');
+        
+        JMenuItem cut=new JMenuItem("CUT");
+        edit.add(cut);
+        cut.addActionListener(this);
+        cut.setActionCommand("CUT");
+        cut.setAccelerator(KeyStroke.getKeyStroke('X', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+        
+        JMenuItem copy=new JMenuItem("COPY");
+        edit.add(copy);
+        copy.setAccelerator(KeyStroke.getKeyStroke('C', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+        copy.setActionCommand("COPY");
+        copy.addActionListener(this);
+        
+        
+        JMenuItem paste=new JMenuItem("PASTE");
+        edit.add(paste);
+        paste.setAccelerator(KeyStroke.getKeyStroke('V', Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+        paste.setActionCommand("PASTE");
+        paste.addActionListener(this);
+        
+        JMenuItem delete=new JMenuItem("DELETE");
+        edit.add(delete);
+        delete.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
+        delete.setActionCommand("DELETE");
+        delete.addActionListener(this);
+        
+        JMenuItem find=new JMenuItem("FIND");
+        edit.add(find);
+        find.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F,KeyEvent.CTRL_MASK));
+        find.setActionCommand("FIND");
+        find.addActionListener(this);
+        
+        JMenuItem replace=new JMenuItem("REPLACE");
+        edit.add(replace);
+        replace.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R,KeyEvent.CTRL_MASK));
+        replace.setActionCommand("REPLACE");
+        replace.addActionListener(this);
+        
+        JMenuItem selectall=new JMenuItem("SELECT ALL");
+        edit.add(selectall);
+        selectall.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A,KeyEvent.CTRL_MASK));
+        selectall.setActionCommand("SELECT ALL");
+        selectall.addActionListener(this);
+        
+        menubar.add(edit);
+        
     }
     
     ArrayList<String> readFile(String filename) throws FileNotFoundException, IOException
@@ -159,9 +218,9 @@ public class Notepad extends JFrame implements ActionListener{
         {
             if ("SAVE".equals(e.getActionCommand()) && isFileOpen)
                 {
-                    ArrayList<String> put=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
+                    screentext=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
                     try {
-                    changeFile(FileOpenAddress,put);
+                    changeFile(FileOpenAddress,screentext);
                     } catch (IOException ex) {
                         Logger.getLogger(Notepad.class.getName()).log(Level.SEVERE, null, ex);
                     }
@@ -177,7 +236,9 @@ public class Notepad extends JFrame implements ActionListener{
         if ("OPEN".equals(e.getActionCommand()))
         {
             int ans=0;
-            if (!isSaved)
+            ArrayList<String> text=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
+                    
+            if (!screentext.equals(text))
             {
                 if (isFileOpen)
                     ans=confirmToSave();
@@ -188,6 +249,7 @@ public class Notepad extends JFrame implements ActionListener{
                     if (option==JOptionPane.CANCEL_OPTION)
                         ans=1;
                     }
+                
             }
             if (ans==0)
             {    
@@ -216,11 +278,13 @@ public class Notepad extends JFrame implements ActionListener{
             }
             isSaved=false;
             }
+            screentext=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
         }
         if ("NEW".equals(e.getActionCommand()))
         {
             int ans=0;
-            if (!isSaved)
+            ArrayList<String> text=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
+            if (!screentext.equals(text))
             {
                 if (isFileOpen)
                     ans=confirmToSave();
@@ -237,12 +301,13 @@ public class Notepad extends JFrame implements ActionListener{
                 npad.setText(null);
                 isSaved=false;
             }
-            
+            screentext=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
         }
         if ("EXIT".equals(e.getActionCommand()))
         {
             int ans=0;
-            if (!isSaved)
+            ArrayList<String> text=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
+            if (!screentext.equals(text))
             {
                 if (isFileOpen)
                     ans=confirmToSave();
@@ -255,9 +320,56 @@ public class Notepad extends JFrame implements ActionListener{
                     
                     }
             }
+            screentext=new ArrayList<>(Arrays.asList((String[])npad.getText().split("\\r?\\n")));
             if (ans==0)
                 System.exit(0);
         }
+        if ("CUT".equals(e.getActionCommand()))
+        {
+            copytext=npad.getSelectedText();
+            npad.setText(npad.getText().replace(npad.getSelectedText(),""));
+        }
+        if ("COPY".equals(e.getActionCommand()))
+        {
+            copytext=npad.getSelectedText();
+            
+        }
+        if ("PASTE".equals(e.getActionCommand()))
+        {
+            npad.insert(copytext, npad.getCaretPosition());
+        }
+        if ("DELETE".equals(e.getActionCommand()))
+        {
+            npad.setText(npad.getText().replace(npad.getSelectedText(),""));
+        }
+        if ("FIND".equals(e.getActionCommand()))
+        {
+            String text=JOptionPane.showInputDialog("Enter the string", null);
+            Highlighter hg=npad.getHighlighter();
+            HighlightPainter painter=new DefaultHighlighter.DefaultHighlightPainter(Color.BLUE);
+            int index=0,start;
+            String x=npad.getText();
+            for (int i=0;i<npad.getText().length();i++)
+            {
+                if ((start=x.indexOf(text, i))!=0)
+                    try {
+                        hg.addHighlight(start, start+text.length() , DefaultHighlighter.DefaultPainter);
+                } catch (BadLocationException ex) {
+                    Logger.getLogger(Notepad.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }    
+        if ("REPLACE".equals(e.getActionCommand()))
+        {
+            JTextField find=new JTextField();
+            JTextField replace=new JTextField();
+            Object[] fandr={"Word to Search",find,"Word to Replace"};
+            String text=JOptionPane.showInputDialog(fandr);
+            Highlighter hg=npad.getHighlighter();
+            int start=npad.getText().indexOf(find.getText());
+            npad.replaceRange(text, start, start+find.getText().length());
+            
+        }  
     }
 
     int confirmToSave()
